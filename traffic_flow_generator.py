8a8a1db0cef04679c87d7ba24a87f7b65925e9dd
Nicolò Santilio <nicolo.santilio@outlook.com>
2024-09-22 11:25:43 +0000

    last commit

---
+++ traffic_flow_generator.py
@@ -0,0 +1,426 @@
+import pandas as pd
+import datetime
+import os
+import json
+import xml.etree.ElementTree as ET
+import random
+
+# Moduli personali
+import tomtom_api
+import processor
+import csv_handler
+import transformer
+
+L_API_KEY = "pAfBQNn77gp9CKIP4Xa3PqTLwsAMQoT2"
+N_API_KEY = "SdbkkAPVV6GxzS7beuYj8mqYnSRWgUmx"
+
+# Funzione per generare il file intermedio con le coordinate degli edge di partenza e arrivo
+def generate_intermediate(edges_file_path, traffic_flows_ref_path, cluster_id, output_path): 
+    csvh = csv_handler.CSVHandler()
+    
+    # Caricamento dei file CSV
+    edges_df = pd.read_csv(edges_file_path)
+    traffic_flows_df = pd.read_csv(f'{traffic_flows_ref_path}/traffic_flows_cluster_{cluster_id}_ref.csv')
+
+    # Rinomina delle colonne del file dei flussi di traffico
+    traffic_flows_df.columns = ['Flow ID', 'Start Edge ID', 'End Edge ID']
+
+    # Unione del file dei flussi con le coordinate degli edge di partenza
+    merged_df = traffic_flows_df.merge(edges_df[['Edge ID', 'From Lat', 'From Lon', 'To Lat', 'To Lon']],
+                                    how='left',
+                                    left_on='Start Edge ID',
+                                    right_on='Edge ID').drop(columns='Edge ID')
+
+    # Rinomina delle colonne per chiarezza
+    merged_df = merged_df.rename(columns={'From Lat': 'Start From Lat', 'From Lon': 'Start From Lon',
+                                        'To Lat': 'Start To Lat', 'To Lon': 'Start To Lon'})
+
+    # Unione del file dei flussi con le coordinate degli edge di arrivo
+    merged_df = merged_df.merge(edges_df[['Edge ID', 'From Lat', 'From Lon', 'To Lat', 'To Lon']],
+                                how='left',
+                                left_on='End Edge ID',
+                                right_on='Edge ID').drop(columns='Edge ID')
+
+    # Rinomina delle colonne per chiarezza
+    merged_df = merged_df.rename(columns={'From Lat': 'End From Lat', 'From Lon': 'End From Lon',
+                                        'To Lat': 'End To Lat', 'To Lon': 'End To Lon'})
+
+    # Salvataggio del file intermedio con le coordinate
+    # intermediate_file_path = '/mnt/data/traffic_flows_with_coordinates.csv'
+    # merged_df.to_csv(intermediate_file_path, index=False)
+
+    # Step 2: Conteggio delle occorrenze di ogni edge come edge di partenza o di arrivo
+    start_counts = traffic_flows_df['Start Edge ID'].value_counts().reset_index()
+    start_counts.columns = ['Edge ID', 'Start Count']
+
+    end_counts = traffic_flows_df['End Edge ID'].value_counts().reset_index()
+    end_counts.columns = ['Edge ID', 'End Count']
+
+    # Unione dei conteggi degli edge di partenza e di arrivo
+    edge_counts_df = pd.merge(start_counts, end_counts, on='Edge ID', how='outer').fillna(0)
+
+    # Salvataggio del file con il conteggio dell'uso degli edge
+    # edge_usage_file_path = '/mnt/data/edge_usage_counts.csv'
+    # edge_counts_df.to_csv(edge_usage_file_path, index=False)
+
+    """
+    # Aggiunta di una colonna ipotetica 'Vehicle Count' con un valore di default
+    merged_df['Vehicle Count'] = 100  # Assegniamo un valore predefinito di 100 per simulazione
+    """
+
+    # Unione dei conteggi degli edge nel file intermedio
+    merged_df = merged_df.merge(edge_counts_df[['Edge ID', 'Start Count']], 
+                                left_on='Start Edge ID', right_on='Edge ID', how='left').drop(columns='Edge ID')
+
+    merged_df = merged_df.merge(edge_counts_df[['Edge ID', 'End Count']], 
+                                left_on='Start Edge ID', right_on='Edge ID', how='left').drop(columns='Edge ID')
+
+    """
+    # Calcolo delle nuove colonne Divided Vehicle Count e Multiplied Vehicle Count
+    merged_df['Divided Vehicle Count'] = merged_df['Vehicle Count'] / merged_df['Start Count']
+    merged_df['Multiplied Vehicle Count'] = merged_df['Vehicle Count'] * merged_df['End Count']
+    """
+
+    # Salvataggio del file aggiornato con le nuove colonne
+    csvh.makePath(f'{output_path}/cluster_{cluster_id}')
+    updated_intermediate_file_path = f'{output_path}/cluster_{cluster_id}/traffic_flow_data_cluster_{cluster_id}.csv'
+    merged_df.to_csv(updated_intermediate_file_path, index=False)
+
+    print(f"File salvato in {updated_intermediate_file_path}")
+    return merged_df
+
+# Funzione per ottenere i dati storici di traffico per ogni giorno
+def get_historic_data(flows_path, cluster_id, day_range, year, month, slots, output_path, from_flow_id = None ):
+    # read the file
+    flows_df = pd.read_csv(f'{flows_path}/output_flows_with_coordinates_cluster_{cluster_id}.csv')
+    
+    # Define a TomTom API Client
+    ttapi = tomtom_api.Client(api_key = N_API_KEY)
+
+    # Define a tomtom processor
+    tt_processor = processor.Processor(ttapi)
+    historid_csv_handler = csv_handler.CSVHandler()
+    
+    
+    # If from_flow_id is 'auto', start from the last flow_id processed, which is the last folder in {output_path}/cluster_{cluster_id}/historic/flow_{flow_id}
+    from_flow_n = from_flow_id
+    lstrip = len(f'flow_{cluster_id}_')
+    if from_flow_id == 'auto' and os.path.exists(f'{output_path}/cluster_{cluster_id}/historic'):
+        flows = next(os.walk(f'{output_path}/cluster_{cluster_id}/historic'))[1]
+        sorted_flows = sorted(flows, key=lambda x: int(x.split('_')[-1]))
+        for flow in sorted_flows:
+            if os.path.exists(f'{output_path}/cluster_{cluster_id}/historic/{flow}'):
+                files = next(os.walk(f'{output_path}/cluster_{cluster_id}/historic/{flow}'))[2]
+                if len(files) > 0:
+                    # get the last date , dates are in format 2024-07-04.csv
+                    dates = [f.split('.')[0] for f in files]
+                    dates.sort()
+                    print (f"Dates: {dates}")
+                    last_date = dates[-1]
+                    _year, _month, _day = last_date.split('-')
+                    if int(_day) < day_range.start:
+                        from_flow_id = flow
+                        break
+            from_flow_id = sorted_flows[-1]
+        else:
+            from_flow_n = 0
+        from_flow_n = int(from_flow_id[lstrip:]) if from_flow_id is not None else 0
+        print (f"Starting from flow_id {from_flow_n}")
+    
+    # Loop per ogni riga nel dataframe e chiamata API
+    for index, row in flows_df.iterrows():
+        flow_id = row['flow_id']
+        
+        # Skip tutti i flow già processati
+        if from_flow_id is not None and int(flow_id[lstrip:]) < from_flow_n:
+            print(f"Skipping flow {flow_id}")
+            continue
+        
+        lat_start = row['start_point_lat']
+        lon_start = row['start_point_lon']
+        lat_end = row['end_point_lat']
+        lon_end = row['end_point_lon']
+        via_1_lat = row['via_point_1_lat']
+        via_1_lon = row['via_point_1_lon']
+        via_2_lat = row['via_point_2_lat']
+        via_2_lon = row['via_point_2_lon']
+        
+        start_point = f"{lat_start},{lon_start}"
+        end_point = f"{lat_end},{lon_end}"
+        via_points = [f"{via_1_lat},{via_1_lon}", f"{via_2_lat},{via_2_lon}"]
+        
+        print (f"Processing flow {flow_id}")
+        for day in day_range:
+            date = datetime.datetime(year, month, day)
+            route_summaries = tt_processor.getHistoricDataOnInterval(start_point, end_point, via_points, date, slots, minute_interval=60)
+            historid_csv_handler.writeHistoricData(route_summaries, date, f'{output_path}/cluster_{cluster_id}/historic/{flow_id}')
+            print(f"Data saved in {output_path}/cluster_{cluster_id}/{flow_id}")
+    print ("All data saved")
+    return    
+
+# Funzione per ottenere i dati di traffico a partire dai dati storici
+def get_traffic_data(traffic_data_path, cluster_id):
+    
+    trs = transformer.Transformer()
+    historid_csv_handler = csv_handler.CSVHandler()
+ 
+    # for each folder in the traffic_data_path
+    for flow in os.listdir(f'{traffic_data_path}/cluster_{cluster_id}/historic'):
+        # for each csv file in the folder
+        for csv in os.listdir(f'{traffic_data_path}/cluster_{cluster_id}/historic/{flow}'):
+            datestr = csv.split('.')[0]
+            # datetime from filename in format yyyy-mm-dd
+            year, month, day = datestr.split('-')
+            date = datetime.datetime(int(year), int(month), int(day))
+            historic_data = historid_csv_handler.readHistoricData(date, f'{traffic_data_path}/cluster_{cluster_id}/historic/{flow}')
+            traffic_data = trs.calculateTrafficData(historic_data)
+            historid_csv_handler.writeTrafficData(traffic_data, date, f'{traffic_data_path}/cluster_{cluster_id}/traffic/{flow}')
+        print (f"[Get Traffic Data] Flow {flow} data saved")
+    print ("[Get Traffic Data] All data saved")
+
+
+# Funzione per ottenere i dati di traffico slottati
+def get_slotted_traffic_data(traffic_data_path, cluster_id, slots):
+
+    trs = transformer.Transformer()
+    historid_csv_handler = csv_handler.CSVHandler()
+    
+    # for each folder in the traffic_data_path
+    for flow in os.listdir(f'{traffic_data_path}/cluster_{cluster_id}/traffic'):
+        # for each csv file in the folder
+        for csv in os.listdir(f'{traffic_data_path}/cluster_{cluster_id}/traffic/{flow}'):
+            datestr = csv.split('.')[0]
+            # datetime from filename in format yyyy-mm-dd
+            year, month, day = datestr.split('-')
+            date = datetime.datetime(int(year), int(month), int(day))
+            traffic_data = historid_csv_handler.readTrafficData(date, f'{traffic_data_path}/cluster_{cluster_id}/traffic/{flow}')
+            slotted_traffic_data = trs.calculateSlottedTraffic(traffic_data, slots)
+            historid_csv_handler.writeSlottedTrafficData(slotted_traffic_data, date, f'{traffic_data_path}/cluster_{cluster_id}/slotted_traffic/{flow}')
+        print (f"[Get Slotted Traffic Data] Slotted traffic for Flow {flow} data saved")
+    print ("[Get Slotted Traffic Data] All data saved")
+
+
+# Funzione per ottenere i dati di traffico aggregati
+def get_aggregated_data(traffic_data_path, cluster_id, slots):
+    trs = transformer.Transformer()
+    historid_csv_handler = csv_handler.CSVHandler()
+    
+    # map of flow : aggregated traffic data
+    aggregated_traffic_data = {}
+    
+    # for each csv of each flow in the slotted_traffic_data_path
+    for flow in os.listdir(f'{traffic_data_path}/cluster_{cluster_id}/slotted_traffic'):
+        # for each csv file in the folder
+        combined_data = historid_csv_handler.readAllSlotted(f'{traffic_data_path}/cluster_{cluster_id}/slotted_traffic/{flow}')
+        agg = trs.getAggregatedTrafficData(combined_data, slots)
+        historid_csv_handler.write(agg, f'{traffic_data_path}/cluster_{cluster_id}/aggregated_traffic/{flow}', 'slotted_traffic_agg.csv')
+        
+        flow_id = flow.split('flow_')[1]
+        # store flow : aggregated traffic data in map
+        aggregated_traffic_data[flow_id] = agg
+        
+        print(f"[Get Aggregated Traffic Data] Aggregated traffic for Flow {flow} data saved")
+    
+    # Convert DataFrames to dictionaries
+    aggregated_traffic_data_dict = {k: v.to_dict() for k, v in aggregated_traffic_data.items()}
+    
+    # Save to JSON file
+    with open(f'{traffic_data_path}/cluster_{cluster_id}/aggregated_traffic_data.json', 'w') as json_file:
+        json.dump(aggregated_traffic_data_dict, json_file)
+    
+    return aggregated_traffic_data
+
+
+def load_aggregated_data(json_file_path):
+    # Load the JSON file
+    with open(json_file_path, 'r') as json_file:
+        aggregated_traffic_data_dict = json.load(json_file)
+    
+    # Convert dictionaries back to DataFrames
+    aggregated_traffic_data = {k: pd.DataFrame(v) for k, v in aggregated_traffic_data_dict.items()}
+    
+    return aggregated_traffic_data
+
+# Funzione per generare i file di traffic flow aggregati per tutti i flussi
+# @begin_values e @end_values sono i valori di inizio e fine dei flussi per la simulazione su SUMO
+def generate_sumo_routes(cluster_id, slot, begin_values = [0, 600, 1200, 2400], end_values = [1200, 2400, 3000, 3600]):
+    # Load the CSV file
+    df = pd.read_csv(f'traffic_flows_data/cluster_{cluster_id}/traffic_flow_data_cluster_{cluster_id}.csv')
+
+    # Load the JSON file
+    with open(f'traffic_flows_data/cluster_{cluster_id}/aggregated_traffic_data.json', 'r') as json_file:
+        aggregated_data = json.load(json_file)
+
+    # Create the root element for the XML
+    routes = ET.Element('routes')
+
+    # Lista temporanea per raccogliere i flow prima di ordinarli
+    flows = []
+
+    # Iterate through each row in the CSV
+    for _, row in df.iterrows():
+        flow_id = row['Flow ID']
+        start_edge_id = row['Start Edge ID']
+        end_edge_id = row['End Edge ID']
+        start_count = row['Start Count']
+        
+        
+        # slot è passato come un time slot in formato hh:mm-hh:mm
+        # bisogna trovare l'indice corrispondente nel dizionario aggregato
+        slot_index = list(aggregated_data[flow_id]['slot'].keys())[list(aggregated_data[flow_id]['slot'].values()).index(slot)]
+        if slot_index is None:
+            print(f"Slot {slot} not found in the aggregated data")
+            return
+
+        if flow_id in aggregated_data and slot_index in aggregated_data[flow_id]['avg_number_of_vehicles']:
+            avg_number_of_vehicles = aggregated_data[flow_id]['avg_number_of_vehicles'][slot_index]
+            number_of_vehicles = (avg_number_of_vehicles / start_count) if start_count != 0 else 0
+            
+            while True:
+                begin = random.choice(begin_values)
+                end = random.choice(end_values)
+                if begin < end:
+                    break
+
+            # Crea il dizionario del flow con tutti i dettagli
+            flow_data = {
+                'id': f'flow_{flow_id}',
+                'from': start_edge_id,
+                'to': end_edge_id,
+                'begin': begin,
+                'end': end,
+                'number': round(number_of_vehicles)
+            }
+            # Aggiungi il flow alla lista
+            flows.append(flow_data)
+
+    # Ordina i flow in base al valore di 'begin'
+    flows.sort(key=lambda x: x['begin'])
+
+    # Aggiungi i flow ordinati all'elemento 'routes'
+    for flow_data in flows:
+        flow = ET.SubElement(routes, 'flow')
+        flow.set('id', flow_data['id'])
+        flow.set('from', flow_data['from'])
+        flow.set('to', flow_data['to'])
+        flow.set('begin', str(flow_data['begin']))
+        flow.set('end', str(flow_data['end']))
+        flow.set('number', str(flow_data['number']))
+
+    # Write the XML to a file
+    tree = ET.ElementTree(routes)
+    ET.indent(tree)
+    fname = f'route_{slot.replace("-","_").replace(":", "-")}'
+    tree.write(f'traffic_flows_data/cluster_{cluster_id}/{fname}.xml', encoding='utf-8', xml_declaration=True)
+
+    print(f"Generated traffic flows saved in traffic_flows_data/cluster_{cluster_id}/{fname}.xml")
+
+
+def reorder_routes(xml_file_path):
+
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
+
+    # Estre tutti i flow dall'XML
+    flows = []
+    for flow in root.findall('flow'):
+        flow_data = {
+            'id': flow.get('id'),
+            'from': flow.get('from'),
+            'to': flow.get('to'),
+            'begin': int(flow.get('begin')),
+            'end': int(flow.get('end')),
+            'number': flow.get('number')
+        }
+        flows.append(flow_data)
+
+    # Ordina i flow in base al valore di 'begin'
+    flows.sort(key=lambda x: x['begin'])
+
+    # Pulisce l'albero XML dai flow esistenti (disordinati)
+    for flow in root.findall('flow'):
+        root.remove(flow)
+
+    # Ricompone l'albero con i flow ordinati 
+    for flow_data in flows:
+        flow = ET.SubElement(root, 'flow')
+        flow.set('id', flow_data['id'])
+        flow.set('from', flow_data['from'])
+        flow.set('to', flow_data['to'])
+        flow.set('begin', str(flow_data['begin']))
+        flow.set('end', str(flow_data['end']))
+        flow.set('number', flow_data['number'])
+
+    # Sovrascrive i file delle routes con i traffic flows ordinati rispetto al begin
+    ET.indent(tree)
+    tree.write(xml_file_path, encoding='utf-8', xml_declaration=True)
+
+
+# Define edge file path, traffic flows path, cluster id, and output path
+edges_file_path = './edges.csv'
+traffic_flows_path = './traffic_flows_ref'
+traffic_flow_coords = './output_flows_coords'
+output_path = './traffic_flows_data'
+
+cluster_id = int(input("Enter cluster id: "))
+
+# Date Range for Route Analysis
+# year = 2024
+# month = 7
+# day_range = range(4, 5)
+
+# year, month and day range come from terminal input
+year = int(input("Enter the year: "))
+month = int(input("Enter the month: "))
+day_range = range(int(input("Enter the start day: ")), int(input("Enter the end day: ")))
+
+# define time slots in a day
+# slots = ["00:00-06:00", "06:00-12:00", "12:00-18:00", "18:00-23:59"]
+slots = ["07:00-10:00", "13:00-15:00", "18:00-21:00"]
+
+"""
+STEP 1
+=====
+A partire dai file CSV dei flow, generare un file intermedio con le coordinate degli edge di partenza e arrivo,
+nonchè il conteggio degli edge di partenza e arrivo per ogni flow
+"""
+# traffic_flow_data_cluster = generate_intermediate(edges_file_path, traffic_flows_path, cluster_id, output_path)
+
+"""
+STEP 2
+=====
+Per ogni flow, chiamare l'API TomTom per ottenere i dati storici di traffico per ogni giorno.
+I dati storici vengono salvati in una cartella separata per ogni flow, non serve eseguire nuovamente questa operazione
+se i dati sono già stati salvati.
+"""
+# get_historic_data(traffic_flow_coords, cluster_id, day_range, year, month, slots, output_path, from_flow_id = 'auto')
+
+"""
+Step 3
+=====
+Per ogni flow, calcolare i dati di traffico a partire dai dati storici.
+"""
+# get_traffic_data(output_path, cluster_id)
+
+"""
+STEP 4 
+=====
+Per ogni flow, calcolare i dati di traffico slottati.
+"""
+# get_slotted_traffic_data(output_path, cluster_id, slots)
+
+"""
+STEP 5
+=====
+Per ogni flow, calcolare i dati di traffico aggregati.
+"""
+# aggregated_traffic_data_for_flow = get_aggregated_data(output_path, cluster_id, slots)
+
+"""
+STEP 6
+=====
+Generare i file di traffic flow aggregati per tutti i flussi
+"""
+generate_sumo_routes(cluster_id, slots[0])
+generate_sumo_routes(cluster_id, slots[1])
+generate_sumo_routes(cluster_id, slots[2])
---
+++ transformer.py
@@ -0,0 +1,92 @@
+import time
+import pandas as pd
+import datetime
+import math
+from dateutil import parser as dtparser
+from IPython.display import display, clear_output
+import os
+
+class Transformer:
+    
+    def __init__(self):
+        pass
+
+    # Calculate traffic data based on route summaries
+    def calculateTrafficData(self, historic_data):
+        route_analysis_df = pd.DataFrame()
+        
+        for index, summary in historic_data.iterrows():
+            clear_output(wait=True)
+            
+            # Departure time
+            datetime = summary['departureTime']
+            
+            # Get length of road
+            length_in_meters = summary['lengthInMeters']
+            # Calculate free flow speed in km/h
+            free_flow_speed = length_in_meters / summary['noTrafficTravelTimeInSeconds']  * 3.6
+            # Calculate traffic speed in km/h
+            traffic_speed = length_in_meters / summary['travelTimeInSeconds']  * 3.6
+            # Calculate traffic density
+            density_factor = free_flow_speed / traffic_speed
+            avg_vehicle_length = 4.6
+            
+            # https://www.amsi.org.au/teacher_modules/pdfs/Maths_delivers/Braking5.pdf
+            breaking_distance = (traffic_speed ** 2) / 20 # b = m/s 
+            
+            # The number of vehicles is calculated as the traffic density * length of road divided by the average length of vehicles and recommended breaking distance
+            vehicles = round(density_factor * length_in_meters / avg_vehicle_length)
+            
+            results = {"datetime": datetime, "free_flow_speed": free_flow_speed, "traffic_speed": traffic_speed, "density_factor": density_factor, "number_of_vehicles": vehicles, "points": summary['points']}
+            
+            # Convert to data frame and append
+            route_analysis_df = pd.concat([route_analysis_df, pd.json_normalize(results)], ignore_index=True)
+            print(f"Computing traffic: { round((index+1) / len(historic_data) * 100) }% >> {datetime}")
+        
+        return route_analysis_df
+
+
+
+    # this function accepts arrays like ["00:00-06:00", "06:00-12:00", "12:00-18:00", "18:00-23:00"]
+    def parseTimeSlot(self, slot):
+        start_time, end_time = slot.split("-")
+        return (dtparser.parse(start_time).time(), dtparser.parse(end_time).time())
+
+    # Calculate cumulated traffic data based on slots, for example ["00:00-06:00", "06:00-12:00", "12:00-18:00", "18:00-23:00"]
+    def calculateSlottedTraffic(self, traffic_data, slots):
+        traffic_data['datetime'] = pd.to_datetime(traffic_data['datetime'])
+        
+        # transform each slot into a time range
+        time_ranges = []
+        for slot in slots:
+            time_ranges.append(self.parseTimeSlot(slot))
+        
+        slot_traffic_df = pd.DataFrame()
+        for slot in time_ranges:
+            # Filter data based on slot
+            slot_data = traffic_data[(traffic_data['datetime'].dt.time >= slot[0]) & (traffic_data['datetime'].dt.time < slot[1])]
+            # Calculate average traffic speed, density and number of vehicles
+            avg_traffic_speed = slot_data['traffic_speed'].mean()
+            avg_density_factor = slot_data['density_factor'].mean()
+            avg_number_of_vehicles = slot_data['number_of_vehicles'].mean()
+            points = slot_data['points'].iat[0]
+            # Append to DataFrame
+            slot_traffic_df = pd.concat([slot_traffic_df, pd.DataFrame({"slot": f"{slot[0]}-{slot[1]}", "avg_traffic_speed": avg_traffic_speed, "avg_density_factor": avg_density_factor, "avg_number_of_vehicles": avg_number_of_vehicles, "points": points }, index=[0])], ignore_index=True)
+        
+        return slot_traffic_df
+    
+    def _aggregateSlottedTrafficBySlot(self, slot_traffic_data, slot):
+        time_slot = self.parseTimeSlot(slot)
+        slot_data = slot_traffic_data[(slot_traffic_data['slot'].str.contains(time_slot[0].strftime('%H:%M')))]
+        avg_traffic_speed = slot_data['avg_traffic_speed'].mean()
+        avg_density_factor = slot_data['avg_density_factor'].mean()
+        avg_number_of_vehicles = slot_data['avg_number_of_vehicles'].mean()
+        points = slot_data['points'].iat[0]
+        return pd.DataFrame({"slot": str(slot), "avg_traffic_speed": avg_traffic_speed, "avg_density_factor": avg_density_factor, "avg_number_of_vehicles": avg_number_of_vehicles, "points": points}, index=[0])
+    
+    def getAggregatedTrafficData(self, slot_traffic_data, slots):
+        aggregated_data = pd.DataFrame()
+        for slot in slots:
+            agg = self._aggregateSlottedTrafficBySlot(slot_traffic_data, slot)
+            aggregated_data = pd.concat([aggregated_data, agg], ignore_index=True)
+        return aggregated_data
